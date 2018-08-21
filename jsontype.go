0be0fc260dc5f437cc886e24fac176fe34d9d8ab
Naoki Yaguchi <murooka.1129.x50@gmail.com>
2018-08-21 21:07:23 +0900

    Initial commit

---
+++ jsontype.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/iancoleman/strcase"
+)
+
+type JSONType struct {
+	Nullable  bool
+	IsBoolean bool
+	IsInteger bool
+	IsNumber  bool
+	IsString  bool
+	Array     *JSONType
+	Object    map[string]*JSONType
+}
+
+func (t *JSONType) ToGoType() string {
+	if t.IsBoolean {
+		return "bool"
+	}
+
+	if t.IsInteger {
+		return "int"
+	}
+
+	if t.IsNumber {
+		return "float64"
+	}
+
+	if t.IsString {
+		return "string"
+	}
+
+	if t.Array != nil {
+		return fmt.Sprintf(`[]%s`, t.Array.ToGoType())
+	}
+
+	if t.Object != nil {
+		s := fmt.Sprintf("struct{\n")
+		for key, typ := range t.Object {
+			s += fmt.Sprintf("%s %s\n", strcase.ToCamel(key), typ.ToGoType())
+		}
+		s += fmt.Sprintf("}\n")
+		return s
+	}
+
+	panic(fmt.Sprintf("cannot serialize type: %#v", t))
+}
+
+func (t *JSONType) Merge(u *JSONType) *JSONType {
+	if t == nil {
+		return u
+	}
+	if u == nil {
+		return t
+	}
+
+	res := &JSONType{}
+	res.Nullable = t.Nullable || u.Nullable
+	res.IsBoolean = t.IsBoolean || u.IsBoolean
+	res.IsInteger = t.IsInteger || u.IsInteger
+	res.IsNumber = t.IsNumber || u.IsNumber
+	res.IsString = t.IsString || u.IsString
+	res.Array = t.Array.Merge(u.Array)
+	res.Object = map[string]*JSONType{}
+	for key, typ := range t.Object {
+		res.Object[key] = res.Object[key].Merge(typ)
+	}
+	for key, typ := range u.Object {
+		res.Object[key] = res.Object[key].Merge(typ)
+	}
+	return res
+}
+
+func detectTypeOfItem(v interface{}) (*JSONType, error) {
+	a, ok := v.([]interface{})
+	if !ok {
+		return nil, fmt.Errorf("root value must be array")
+	}
+
+	var typ *JSONType = nil
+	for _, e := range a {
+		typ = typ.Merge(detectType(e))
+	}
+
+	return typ, nil
+}
+
+func detectType(v interface{}) *JSONType {
+	t := &JSONType{}
+
+	switch v := v.(type) {
+	case map[string]interface{}:
+		t.Object = map[string]*JSONType{}
+		for key, val := range v {
+			t.Object[key] = t.Object[key].Merge(detectType(val))
+		}
+	case []interface{}:
+		for _, val := range v {
+			t.Array = t.Array.Merge(detectType(val))
+		}
+	case string:
+		t.IsString = true
+	case float64:
+		// TODO
+		t.IsNumber = true
+	case bool:
+		t.IsBoolean = true
+	case nil:
+		t.Nullable = true
+	}
+
+	return t
+}
---
+++ main.go
@@ -0,0 +1,169 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"go/format"
+	"io"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/iancoleman/strcase"
+	"github.com/jessevdk/go-flags"
+)
+
+func main() {
+	var opts struct {
+		Output   string `long:"output" default:"-"`
+		TypeName string `long:"typename"`
+		VarName  string `long:"varname"`
+		Package  string `long:"package"`
+	}
+
+	args, err := flags.Parse(&opts)
+	if err != nil {
+		if err := err.(*flags.Error); err.Type == flags.ErrHelp {
+			os.Exit(1)
+		} else {
+			log.Fatalf("failed to parse arguments: %s", err)
+		}
+	}
+
+	if len(args) == 0 {
+		log.Fatal("argument is not found")
+	} else if len(args) > 1 {
+		log.Fatalf("too many arguments: %v", args)
+	}
+
+	if opts.Package == "" {
+		log.Fatal("--package must be specifed")
+	}
+
+	if opts.TypeName == "" {
+		log.Fatal("--typename must be specified")
+	}
+
+	if opts.VarName == "" {
+		log.Fatal("--varname must be specified")
+	}
+
+	filename := args[0]
+	v, err := loadJSON(filename)
+	if err != nil {
+		log.Fatalf("failed to parse JSON: %s", err)
+	}
+
+	typ, err := detectTypeOfItem(v)
+	if err != nil {
+		log.Fatalf("failed to detect JSON type: %s", err)
+	}
+
+	g := NewGenerator()
+	g.Printlnf("package %s", opts.Package)
+	g.Printlnf("")
+	g.Printlnf("type %s %s", opts.TypeName, typ.ToGoType())
+	g.Printlnf("")
+
+	g.Printlnf("var %s = []%s {", opts.VarName, opts.TypeName)
+	a := v.([]interface{})
+	for _, e := range a {
+		g.Printlnf(toLiteral(e, typ) + ",")
+	}
+	g.Printlnf("}")
+
+	src, err := g.Generate()
+	if err != nil {
+		log.Fatalf("failed to format output source code: %s", err)
+	}
+
+	var out io.Writer
+	if opts.Output == "-" {
+		out = os.Stdout
+	} else {
+		out, err = os.Create(opts.Output)
+		if err != nil {
+			log.Fatalf("failed to open file: %s", err)
+		}
+	}
+
+	out.Write(src)
+}
+
+func toLiteral(v interface{}, typ *JSONType) string {
+	switch v := v.(type) {
+	case map[string]interface{}:
+		if typ.Object == nil {
+			panic("assertion error")
+		}
+
+		buf := &bytes.Buffer{}
+		buf.WriteString("{\n")
+		for k, e := range v {
+			ctyp := typ.Object[k]
+			buf.WriteString(fmt.Sprintf("%s: %s,\n", strcase.ToCamel(
+				k), toLiteral(e, ctyp)))
+		}
+		buf.WriteString("}")
+		return buf.String()
+	case []interface{}:
+		if typ.Array == nil {
+			panic("assertion error")
+		}
+
+		buf := &bytes.Buffer{}
+		buf.WriteString(fmt.Sprintf("[]%s{\n", typ.Array.ToGoType()))
+		for _, e := range v {
+			buf.WriteString(toLiteral(e, typ.Array))
+			buf.WriteString(",\n")
+		}
+		buf.WriteString("}")
+		return buf.String()
+	case string:
+		return strconv.Quote(v)
+	case float64:
+		// TODO
+		return strconv.FormatFloat(v, 'f', -1, 64)
+	case bool:
+		return strconv.FormatBool(v)
+	case nil:
+		return "null"
+	}
+
+	panic(fmt.Sprintf("unknown type of value: %#v", v))
+}
+
+func loadJSON(filename string) (interface{}, error) {
+	f, err := os.Open(filename)
+	if err != nil {
+		log.Fatalf("failed to open file: %s", err)
+	}
+	defer f.Close()
+
+	var v interface{}
+	err = json.NewDecoder(f).Decode(&v)
+	if err != nil {
+		log.Fatalf("failed to decode JSON: %s", err)
+	}
+
+	return v, nil
+}
+
+type Generator struct {
+	buf *bytes.Buffer
+}
+
+func NewGenerator() *Generator {
+	return &Generator{&bytes.Buffer{}}
+}
+
+func (g *Generator) Printlnf(format string, args ...interface{}) {
+	g.buf.WriteString(fmt.Sprintf(format, args...))
+	g.buf.WriteByte('\n')
+}
+
+func (g *Generator) Generate() ([]byte, error) {
+	return format.Source(g.buf.Bytes())
+	return g.buf.Bytes(), nil
+}
